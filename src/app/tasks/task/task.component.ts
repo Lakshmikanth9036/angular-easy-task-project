44a94bba170f31c2d5a3d1684c328195301294be
Lakshmikanth9036 <kanth9036.1997@gmail.com>
2024-06-01 16:54:13 +0530

    Migrating project from standalone to module based.

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,13 +1,9 @@
 import { Component, Input, inject } from '@angular/core';
-import { Task } from './task.model';
-import { CardComponent } from '../../shared/card/card.component';
-import { DatePipe } from '@angular/common';
 import { TaskService } from '../tasks.service';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-task',
-  standalone: true,
-  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
